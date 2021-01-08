fbc6d77fc6215401cd130071da8e21f13f0a6d22
Myeongjung <31848514+Myeongjung@users.noreply.github.com>
2021-01-08 16:36:15 +0100

    Edit one error
    Edit one error

--- Python/Solution#15-21.py
+++ Python/Solution#15-21.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         
     print(any(c.isalnum() for c in s))
     print(any(c.isalpha() for c in s))
-    print(any(c.isdigit() for c in ss))
+    print(any(c.isdigit() for c in s))
     print(any(c.islower() for c in s))
     print(any(c.isupper() for c in s))
 	
