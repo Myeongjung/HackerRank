e8bb2e07e1e5c072db94a924e0f8f39e3f1e744b
MYEONGJUNG\MJ Park <92mj.park@gmail.com>
2021-01-09 20:51:09 +0100

    Start warm-up challenges

---
+++ Interview Preparation Kit/Warm-up Challenges/Sales by Match.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    count = 0
+    arr = set(ar)
+    for v in arr:
+        count += ar.count(v)//2
+    return count            
+            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
