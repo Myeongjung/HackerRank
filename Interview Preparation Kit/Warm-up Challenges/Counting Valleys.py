d40967fcf8a4635a51ccfe8a2b026c9a70da3c16
MYEONGJUNG\MJ Park <92mj.park@gmail.com>
2021-01-09 21:27:30 +0100

    Added one more file

---
+++ Interview Preparation Kit/Warm-up Challenges/Counting Valleys.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    sea_level = 0
+    count = 0
+    for i in range (steps):
+        if path[i] == "U":
+            sea_level += 1
+            if sea_level == 0:
+                count += 1
+        else:
+            sea_level -= 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
