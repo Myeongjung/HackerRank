95dc1d8cabb785ba4f4906389f7c827596643f03
MYEONGJUNG\MJ Park <92mj.park@gmail.com>
2021-01-14 16:01:12 +0100

    add questions

--- SQL/Solution #3.sql
+++ SQL/Solution #3.sql
@@ -37,3 +37,99 @@ GROUP BY 1
 ORDER BY EARNINGS DESC
 LIMIT 1
 
+--39. Asian Population
+SELECT SUM(A.POPULATION)
+FROM CITY A 
+    JOIN COUNTRY B
+    ON A.COUNTRYCODE = B.CODE
+WHERE B.CONTINENT = 'ASIA'
+
+--40. African Cities
+SELECT A.NAME
+FROM CITY A 
+    JOIN COUNTRY B
+    ON A.COUNTRYCODE = B.CODE
+WHERE B.CONTINENT = 'AFRICA'
+
+--41. Average Population of Each Continent
+SELECT B.CONTINENT, FLOOR(AVG(A.POPULATION))
+FROM CITY A 
+    JOIN COUNTRY B
+    ON A.COUNTRYCODE = B.CODE
+GROUP BY CONTINENT
+
+--42. Draw The Triangle 1
+SET @i := 21;
+SELECT REPEAT('* ', @i := @i - 1)
+FROM INFORMATION_SCHEMA.TABLES;
+
+--43. Draw The Triangle 2
+SET @i := 0;
+SELECT REPEAT('* ', @i := @i + 1)
+FROM INFORMATION_SCHEMA.TABLES;
+WHERE @i < 20;
+
+--44. The PADS
+SELECT CONCAT(NAME, '(', SUBSTR(OCCUPATION, 1,1), ')')
+FROM OCCUPATIONS
+ORDER BY NAME;
+
+SELECT CONCAT('There are a total of', ' ', COUNT(*), ' ', LOWER(OCCUPATION), 's.')
+FROM OCCUPATIONS
+GROUP BY OCCUPATION
+ORDER BY COUNT(*), OCCUPATION
+
+--45. Occupations
+set @r1=0, @r2=0, @r3=0, @r4=0;
+select min(Doctor), min(Professor), min(Singer), min(Actor)
+from(
+  select case when Occupation='Doctor' then (@r1:=@r1+1)
+            when Occupation='Professor' then (@r2:=@r2+1)
+            when Occupation='Singer' then (@r3:=@r3+1)
+            when Occupation='Actor' then (@r4:=@r4+1) end as RowNumber,
+    case when Occupation='Doctor' then Name end as Doctor,
+    case when Occupation='Professor' then Name end as Professor,
+    case when Occupation='Singer' then Name end as Singer,
+    case when Occupation='Actor' then Name end as Actor
+  from OCCUPATIONS
+  order by Name
+) temp
+group by RowNumber;
+
+SELECT
+    GROUP_CONCAT(if(Occupation = 'Doctor', Name, NULL)) AS 'Doctor', 
+    GROUP_CONCAT(if(Occupation = 'Professor', Name, NULL)) AS 'Professor', 
+    GROUP_CONCAT(if(Occupation = 'Singer', Name, NULL)) AS 'Singer', 
+    GROUP_CONCAT(if(Occupation = 'Actor', Name, NULL)) AS 'Actor'
+FROM
+(
+    SELECT a.*,
+        (CASE @vOccup WHEN a.OCCUPATION THEN @rownum:=@rownum+1 ELSE @rownum:=1 END) rnum,
+        (@vOccup:=a.OCCUPATION) vOccup
+    FROM OCCUPATIONS a,(SELECT @vOccup:='', @rownum:=0 FROM DUAL) b
+    ORDER BY OCCUPATION, NAME ) c
+GROUP BY c.rnum
+
+--46. Binary Tree Nodes
+SELECT
+    CASE WHEN P IS NULL THEN CONCAT(N, ' Root')
+    WHEN N IN (SELECT DISTINCT P FROM BST) THEN CONCAT(N, ' Inner')
+    ELSE CONCAT(N, ' Leaf')
+    END
+FROM BST
+ORDER BY N
+
+--47. New Companies
+SELECT  c.COMPANY_CODE, 
+    c.FOUNDER,
+    DISTINCT COUNT(l.lead_manager_code),
+    DISTINCT COUNT(s.senior_manager_code),
+    DISTINCT COUNT(m.manager_code),
+    DISTINCT COUNT(e.employee_code)
+FROM  Company c, Lead_Manager l, Senior_Manager s, Manager m, Employee e 
+WHERE c.company_code = l.company_code 
+    AND l.lead_manager_code=s.lead_manager_code 
+    AND s.senior_manager_code=m.senior_manager_code 
+    AND m.manager_code=e.manager_code 
+GROUP BY c.company_code
+ORDER BY c.company_code;
