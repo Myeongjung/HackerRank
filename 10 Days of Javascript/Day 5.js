ab1a4bac31375be6b4fd09fdd4341e271c3add25
MYEONGJUNG\MJ Park <92mj.park@gmail.com>
2021-02-14 13:48:03 +0100

    solved day 4-5

--- 10 Days of Javascript/Day 5.js
+++ 10 Days of Javascript/Day 5.js
@@ -1,4 +1,26 @@
-// Day 0: Hello, World!
+// Day 5: Inheritance
+Rectangle.prototype.area = function(){
+    return this.w * this.h;
+}
 
+class Square extends Rectangle{
+    constructor(w){
+        super(w,w);
+    }
+}
 
-// Day 0:
+// Day 5: Template Literals
+function sides(literals, ...expressions) {
+    var area = expressions[0];
+    var perimeter = expressions[1];
+    
+    var s1 = (perimeter + Math.sqrt(perimeter**2 - (16*area)))/4;
+    var s2 = (perimeter - Math.sqrt(perimeter**2 - (16*area)))/4;
+    
+    return [s1, s2].sort();
+}
+
+// Day 5: Arrow Functions
+function modifyArray(nums) {
+    return (nums.map(num => (num%2==0)?num*2:num*3))
+}
--- 10 Days of Javascript/Day 6.js
+++ 10 Days of Javascript/Day 6.js
@@ -1,4 +1,19 @@
-// Day 0: Hello, World!
+// Day 6: Bitwise Operators
+function getMaxLessThanK(n,k){
+    var max = 0;
+    for(let i = 1 ; i < n+1 ; i++){
+        for(let j = i+1 ; j < n+1 ; j++){
+            if(((i&j) > max) && ((i&j) < k)) {
+                max = (i&j);
+            }
+        }
+    }
+    return max;
+}
 
-
-// Day 0:
+// Day 6: JavaScript Dates
+function getDayName(dateString) {
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    return weekdays[new Date(dateString).getDay()];
+}
