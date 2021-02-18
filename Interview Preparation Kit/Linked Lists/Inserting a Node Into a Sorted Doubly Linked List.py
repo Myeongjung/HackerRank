0a492dadfa99c2e62c2580f37a11d373bf209ad3
MYEONGJUNG\MJ Park <92mj.park@gmail.com>
2021-02-18 19:05:28 +0100

    add solutions

---
+++ Interview Preparation Kit/Linked Lists/Inserting a Node Into a Sorted Doubly Linked List.py
@@ -0,0 +1,94 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the sortedInsert function below.
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+# 2 3 4 ,1
+def sortedInsert(head, data):
+    insertNode = DoublyLinkedListNode(data)
+    if(head.data < data):
+        if head.next != None:
+            sortedInsert(head.next, data)
+        else:
+            head.next = insertNode
+            insertNode.prev = head
+    else:
+        if head.prev != None:
+            prev = head.prev
+            prev.next = insertNode
+            head.prev = insertNode
+            insertNode.next = head
+            insertNode.prev = prev
+        else:
+            insertNode.next = head
+            head.prev = insertNode
+            head = insertNode
+    return head
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        data = int(input())
+
+        llist1 = sortedInsert(llist.head, data)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
---
+++ Interview Preparation Kit/Trees/Height of a Binary Tree.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+
+def height(root):
+    left_h = 0
+    right_h = 0
+
+    if root.left != None:
+        left_h = 1 + height(root.left)
+
+    if root.right != None:
+        right_h = 1 +  height(root.right)
+        
+    return (left_h if left_h > right_h else right_h)
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(height(tree.root))
