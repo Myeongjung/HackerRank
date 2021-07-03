d7c52b6a016563cae571466a779027a01817abf6
Myeongjung <31848514+Myeongjung@users.noreply.github.com>
2021-07-03 14:48:25 +0200

    add solution 17-21

--- Problem Solving/Solution #17-31.py
+++ Problem Solving/Solution #17-31.py
@@ -1,12 +1,46 @@
-#17. 
+#17. Subarray Division
+def birthday(s, d, m):
+    count = 0
+    for i in range(len(s)-m+1):
+        if sum(s[i:i+m]) == d:
+            count += 1
+    return count
 
-#18. 
+#18. Divisible Sum Pairs
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if (ar[i] + ar[j]) % k == 0:
+                count += 1
+                
+    return count
 
-#19. 
+#19. Migratory Birds
+from collections import Counter
 
-#20. 
+def migratoryBirds(arr):
+    c = Counter(arr)
+    result = freq = 0
+    for key, value in sorted(c.items()):           
+        if value > freq:
+            result = key
+            freq = value
+            
+    return result
 
-#21. 
+#20. Day of the Programmer
+def dayOfProgrammer(year):
+    if year%4==0 and (year<1918 or year%400==0 or year%100!=0):
+        return "12.09.%s" % year
+    elif year == 1918:
+        return "26.09.1918"
+    return "13.09.%s" % year
+
+#21. Bill Division
+def bonAppetit(bill, k, b):
+    shared = (sum(bill) - bill[k])//2
+    print(b-shared if shared != b else "Bon Appetit")
 
 #22. 
 
