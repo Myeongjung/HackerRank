0929bfc82daad37ed8424868d6b0a10524c7d763
MYEONGJUNG\MJ Park <92mj.park@gmail.com>
2021-12-26 00:01:52 +0900

    no message

--- Databases/Solution 5.py
+++ Databases/Solution 5.py
@@ -41,4 +41,56 @@ if __name__ == '__main__':
   for line in sys.stdin:
    inputData.append(line)
   mapReducer.execute(inputData, mapper, reducer)
- 
+  
+  
+# Map Reduce Advanced - Relational Join
+
+import sys
+from collections import OrderedDict
+class MapReduce:
+    def __init__(self):
+        self.intermediate = OrderedDict()
+        self.result = []
+   
+
+    def emitIntermediate(self, key, value):
+   	self.intermediate.setdefault(key, [])       
+        self.intermediate[key].append(value)
+
+    def emit(self, value):
+        self.result.append(value) 
+
+    def execute(self, data, mapper, reducer):
+        for record in data:
+            mapper(record)
+
+        for key in self.intermediate:
+            reducer(key, self.intermediate[key])
+
+        self.result.sort()
+        for item in self.result:
+            print item
+
+mapReducer = MapReduce()
+
+def mapper(record):
+    words = record.rstrip().split(',')
+    if words[0]=='Department':
+        mapReducer.emitIntermediate(words[1],('D',words[2]))
+    elif words[0]=='Employee':
+        mapReducer.emitIntermediate(words[2],('P',words[1]))
+        
+def reducer(key, list_of_values):
+    list_of_values.sort()
+    name = list_of_values[-1][-1]
+    for d in list_of_values:
+        if d[0]!='P':
+            mapReducer.emit((key,name, d[-1]))
+
+if __name__ == '__main__':
+  inputData = []
+  for line in sys.stdin:
+   inputData.append(line)
+  mapReducer.execute(inputData, mapper, reducer)
+  
+  
